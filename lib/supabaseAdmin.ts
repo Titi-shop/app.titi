9ff104f3e82ae19b0fce6ca69730b0b4aacb46e9
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-04-09 00:09:39 +0700

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -6,9 +6,9 @@ import { createClient } from "@supabase/supabase-js";
  * Dùng cho API route, không import vào client
  */
 export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  {
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
     auth: {
       persistSession: false,
       autoRefreshToken: false,
