16a33551505132fc01e235269fb0db09b5381da6
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-04-10 00:26:35 +0700

    Update page.tsx

--- app/seller/stock/page.tsx
+++ app/seller/stock/page.tsx
@@ -281,25 +281,6 @@ export default function SellerStockPage() {
     }
   };
 
-  /* =========================
-     LOADING
-  ========================= */
-   const now = new Date();
-      return (
-        <div
-          key={i}
-          className="flex gap-3 p-3 bg-white rounded-xl shadow animate-pulse"
-        >
-          <div className="w-24 h-24 bg-gray-200 rounded-lg" />
-
-          <div className="flex-1 space-y-2">
-            <div className="h-4 bg-gray-200 rounded w-3/4" />
-            <div className="h-4 bg-gray-200 rounded w-1/2" />
-            <div className="h-3 bg-gray-200 rounded w-1/3" />
-          </div>
-        </div>
-      );
-    }
 
   /* =========================
      UI
