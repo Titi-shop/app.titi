ead950bdd5f29c9caa6880fb4d84f473f351b773
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-04-09 15:57:54 +0700

    Update PiRootClient.tsx

--- app/PiRootClient.tsx
+++ app/PiRootClient.tsx
@@ -13,7 +13,7 @@ export default function PiRootClient({ children }: { children: React.ReactNode }
       <PiProvider />
       <Toaster position="top-center" reverseOrder={false} />
       <Navbar />
-      <main className="pt-[72px] bg-white">
+      <main className="bg-white">
         {children}
       </main>
       <BottomNav />
