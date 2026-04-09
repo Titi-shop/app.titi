fdce432e02e970a7a17c7f1280a0f603a40f0847
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-04-09 15:51:48 +0700

    Update PiRootClient.tsx

--- app/PiRootClient.tsx
+++ app/PiRootClient.tsx
@@ -6,20 +6,17 @@ import Navbar from "@/components/Navbar";
 import BottomNav from "@/components/BottomNav";
 import PiProvider from "@/app/pi/PiProvider";
 import "@/app/lib/i18n";
-import { AuthProvider } from "@/context/AuthContext";
 
 export default function PiRootClient({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <PiProvider />
-        <Toaster position="top-center" reverseOrder={false} />
-        <Navbar />
-        <main className="pt-[72px] bg-white">
-          {children}
-        </main>
-        <BottomNav />
-      </CartProvider>
-    </AuthProvider>
+    <CartProvider>
+      <PiProvider />
+      <Toaster position="top-center" reverseOrder={false} />
+      <Navbar />
+      <main className="pt-[72px] bg-white">
+        {children}
+      </main>
+      <BottomNav />
+    </CartProvider>
   );
 }
