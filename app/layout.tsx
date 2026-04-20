36d3ef7fc9bf2bd56445d83329b994a39534d5ca
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-04-20 22:55:25 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,24 @@ import PiRootClient from "./PiRootClient";
 import { AuthProvider } from "@/context/AuthContext";
 import AlertProvider from "@/app/components/AlertProvider";
 import { SWRConfig } from "swr";
+import ThemeProvider from "@/components/ThemeProvider"; // ✅ THÊM
 
 export const metadata = {
   title: "aliali",
   description: "Ứng dụng thương mại điện tử thanh toán qua Pi Network Testnet",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="vi">
       <head>
-        {/* 🔥 preload ảnh (QUAN TRỌNG) */}
-  <link rel="preload" as="image" href="/avatar.png" />
-  <link rel="preload" as="image" href="/banners/default-shop.png" />
+        <link rel="preload" as="image" href="/avatar.png" />
+        <link rel="preload" as="image" href="/banners/default-shop.png" />
+
         <Script
           src="https://sdk.minepi.com/pi-sdk.js"
           strategy="afterInteractive"
@@ -33,10 +38,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         >
           <AlertProvider />
           <AuthProvider>
-           <ThemeProvider>
+            <ThemeProvider>
               <PiRootClient>{children}</PiRootClient>
             </ThemeProvider>
           </AuthProvider>
+
         </SWRConfig>
       </body>
     </html>
