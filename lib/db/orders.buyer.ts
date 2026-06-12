c0fab22d9a9bad630d75c72ef056f6766f85dc16
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-06-12 11:24:52 +0700

    Update orders.buyer.ts

--- lib/db/orders.buyer.ts
+++ lib/db/orders.buyer.ts
@@ -218,7 +218,7 @@ export async function getOrderByBuyerId(
 
         o.payment_status,
         o.fulfillment_status,
-       MAX(rt.status) AS return_status,
+        rt.status AS return_status
         o.total,
         o.currency,
 
