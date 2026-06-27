11197ddf7e786bf2a1e67d5eb8fc149e9e002c58
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-06-27 18:40:02 +0700

    Update settlement.event.a2u.ts

--- lib/db/settlement/settlement.event.a2u.ts
+++ lib/db/settlement/settlement.event.a2u.ts
@@ -44,27 +44,14 @@ export type WithdrawalSettlementEventInput =
    EVENTS
 ===================================================== */
 
-export const WithdrawalSettlementEvents = {
-
-  WITHDRAW_APPROVED:
-    "WITHDRAW_APPROVED",
-
-  WITHDRAW_PROCESSING:
-    "WITHDRAW_PROCESSING",
-
-  JOURNAL_CREATED:
-    "JOURNAL_CREATED",
-
-  WITHDRAW_COMPLETED:
-    "WITHDRAW_COMPLETED",
-
-  BALANCE_RELEASED:
-    "BALANCE_RELEASED",
-
-  JOURNAL_REVERTED:
-    "JOURNAL_REVERTED",
-
-} as const;
+export enum WithdrawalSettlementEvents {
+  WITHDRAW_APPROVED = "WITHDRAW_APPROVED",
+  WITHDRAW_PROCESSING = "WITHDRAW_PROCESSING",
+  JOURNAL_CREATED = "JOURNAL_CREATED",
+  WITHDRAW_COMPLETED = "WITHDRAW_COMPLETED",
+  BALANCE_RELEASED = "BALANCE_RELEASED",
+  JOURNAL_REVERTED = "JOURNAL_REVERTED",
+}
 /* =====================================================
    LOG
 ===================================================== */
